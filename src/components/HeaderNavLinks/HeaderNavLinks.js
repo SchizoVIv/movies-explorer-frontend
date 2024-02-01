d72d03af12a92dfe4bcd6c0dd5fced6ff1833fd6
Vlada Yakowleva <vladyakowleva@yandex.ru>
2024-02-01 20:39:05 +0200

    fix: navigate link

--- src/components/HeaderNavLinks/HeaderNavLinks.js
+++ src/components/HeaderNavLinks/HeaderNavLinks.js
@@ -12,20 +12,13 @@ function HeaderNavLinks() {
     setMobile(!mobile);
   }
 
-  // let headerLinkClass = 'header__link link-line';
-  // if (mobile) {
-  //   headerLinkClass = 'header__link header__link_active';
-  // } else {
-  //   headerLinkClass += ' header__link_black ';
-  // }
-
   let headerLinkClass = 'header__link link-line';
   if (mobile) {
     headerLinkClass += ' header__link_active';
   } else if (!mobile && window.location.pathname === !'/saved-movies') {
     headerLinkClass += ' header__link_black';
   } else if (!mobile && window.location.pathname === '/saved-movies') {
-    headerLinkClass += ' header__link_black ';
+    headerLinkClass += ' header__link_black';
   }
 
     return (
